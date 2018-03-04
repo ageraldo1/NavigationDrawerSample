4c8b9b77ad879d4b40e1afa346b649de0a358c33
ageraldo1 <ageraldo@itktechnologies.com>
2018-03-04 12:21:35 -0500

    mood & quotes
    mood & quotes added

--- app/src/main/java/com/itktechnologies/manicdepressionsuppression/Mood.java
+++ app/src/main/java/com/itktechnologies/manicdepressionsuppression/Mood.java
@@ -5,17 +5,50 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TabHost;
 
 /**
  * Created by Alexandre on 2/25/2018.
  */
 
 public class Mood extends android.support.v4.app.Fragment {
+
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         getActivity().setTitle("Mood");
+
+        TabHost tabHost = (TabHost) view.findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        // tab1
+        TabHost.TabSpec spec = tabHost.newTabSpec("Anger");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Anger");
+        tabHost.addTab(spec);
+
+
+        // tab2
+        spec = tabHost.newTabSpec("Sadness");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Sadness");
+        tabHost.addTab(spec);
+
+
+        // tab3
+        spec = tabHost.newTabSpec("Happy");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Happy");
+        tabHost.addTab(spec);
+
+
+
+
+
+
     }
 
     @Nullable
--- app/src/main/java/com/itktechnologies/manicdepressionsuppression/Quotes.java
+++ app/src/main/java/com/itktechnologies/manicdepressionsuppression/Quotes.java
@@ -5,17 +5,51 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by Alexandre on 2/25/2018.
  */
 
 public class Quotes extends android.support.v4.app.Fragment {
+    protected ListView mListView;
+    protected int[] images = { R.drawable.jan,
+            R.drawable.feb,
+            R.drawable.mar,
+            R.drawable.apr,
+            R.drawable.may,
+            R.drawable.jun,
+            R.drawable.jul,
+            R.drawable.aug,
+            R.drawable.sep,
+            R.drawable.oct,
+            R.drawable.nov,
+            R.drawable.dec
+    };
+    private String[] monthNames = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         getActivity().setTitle("Quotes");
+
+        mListView = view.findViewById(R.id.listView);
+        CustomAdaptor customAdaptor = new CustomAdaptor();
+        mListView.setAdapter(customAdaptor);
+
+
     }
 
     @Nullable
@@ -25,4 +59,35 @@ public class Quotes extends android.support.v4.app.Fragment {
 
     }
 
+    class CustomAdaptor extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View v = getLayoutInflater().inflate(R.layout.quotes_custom_layout, null);
+            ImageView imageView = v.findViewById(R.id.imageView);
+            TextView textView = v.findViewById(R.id.textView);
+
+            imageView.setImageResource(images[i]);
+            textView.setText(monthNames[i]);
+
+            return v;
+        }
+    }
+
 }
+
